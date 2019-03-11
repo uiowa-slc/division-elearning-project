314e237171413c82438717482644435bc5c622df
Jessica Brown <jessica-e-brown@uiowa.edu>
2019-03-11 13:38:07 -0500

    add next page functionality

--- code/ElearningCourseQuiz.php
+++ code/ElearningCourseQuiz.php
@@ -20,6 +20,30 @@ class ElearningCourseQuiz extends ElearningCoursePage {
 	    $fields->addFieldToTab('Root.Main', new TextField('QuizURL', 'Qualatrics/Quiz URL'), 'QuizURL');
 
 	    return $fields;
-	  }
+	}
+
+	public function Course(){
+		$pageTemp = $this;
+		while($pageTemp->ClassName != 'ElearningCourseHome'){
+			$pageTemp = $this->getParent();
+		}
+		return $pageTemp;
+	}
+
+	
+	public function getNextPage() {
+		//If part has children, then the next page is its first child.
+		if($this->Children()->First()){
+			return $this->Children()->First();
+		}else{
+			$page = Page::get()->filter( array (
+				'ParentID' => $this->ParentID,
+				'Sort:GreaterThan' => $this->Sort
+			) )->First();
+		}
+		if(isset($page)){
+			return $page;
+		}
+	}
 	  
 }
